f616eea68a2286b666c4ded91d23bfa3532288df
Borhaan Hnia <borhaan@Borhaans-MacBook-Pro.local>
2018-11-09 17:39:40 -0500

    last commit

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -4,18 +4,59 @@ import Checkout from './Checkout'
 
 class ShoppingCart extends Component {
     
-    showItems(item){
+   
+    constructor() {
+        super()
+        this.state = {
+            
+        }
+    //     this.increaseItem = this.increaseItem.bind(this)
+    //     this.decreaseItem = this.decreaseItem.bind(this)
+        this.getTotal = this.getTotal.bind(this)
+    }
+    // increaseItem(event) {
+    //     event.preventDefault()
+    //     let newQuantity = this.state.quantity + 1
+    //     this.setState({ quantity: newQuantity })
+
+    // }
+    // decreaseItem(event) {
+    //     event.preventDefault()
+    //     let newQuantity = this.state.quantity - 1
+    //     this.setState({ quantity: newQuantity })
+    // }
+   
+    getTotal(){
+        let total =0
+        this.props.items.forEach(function(item){
+            let price =parseInt(item.price)
+            total += price
+        });
+        return `${total} $`
+        // for(let i=0; i<items.length; i++){
+        //     total += items[i].price
+        //     return total
+    
+}
+    showItems(item) {
+      
+       
         return(<div>
             <img src={'/' + item.image}></img>
             <div>Name: {item.name}</div>
             <div>Description: {item.description}</div>
             <div>Price: {item.price}$</div>
+
+            
             </div>)
     }
     render() {
         return (<div className="shoppingCart">
             <div>{this.props.items.map(this.showItems)}</div>
-            <Checkout/>
+            {/* <input type="submit" value="+" onClick={this.increaseItem} />
+            <input type="submit" value="-" onClick={this.decreaseItem} /> */}
+            <div>Total price:{this.getTotal(this.props.items)}</div>
+            <Checkout />
         </div>)
     }
 }
